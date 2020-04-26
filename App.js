2ef0f24129ac0c52ecc6b56a615a553bd09484c7
calumgould <calumgould26@gmail.com>
2020-04-26 18:08:31 +0100

    boi displaying

---
+++ App.js
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.body}>
+        <Text style={styles.whiteText}>Habbit.</Text>
+        <Image style={styles.image} source={require('./assets/images/boi1_small.png')} />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'slategrey',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: 'ghostwhite',
+    fontSize: 36,
+    fontFamily: 'ArcadeClassic',
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+});
+
+export default App;
