5915ed6fe4a7edde3650876d22497042731caafa
calumgould <calumgould26@gmail.com>
2020-04-27 17:28:22 +0100

    working on pet container

--- components/StepsComponent.js
+++ components/StepsComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import * as Progress from 'react-native-progress';
+import { TextInput, StyleSheet, TouchableOpacity } from 'react-native-gesture-handler';
 
 
 class StepsComponent extends Component {
@@ -12,22 +13,49 @@ class StepsComponent extends Component {
             stepGoal: 5000
          }
          this.calculateProgress = this.calculateProgress.bind(this)
+         this.handleStepChange = this.handleStepChange.bind(this)
     }
 
     calculateProgress(){
         return this.state.dailySteps / this.state.stepGoal
     }
 
+    handleStepChange(event) {
+        this.setState({
+            totalSteps: this.state.totalSteps + event.target.value,
+            dailySteps: event.target.value
+        })
+    }
     
 
     render() { 
         return ( 
             <View>
-                <Text>{this.state.totalSteps} / {this.state.stepGoal}</Text>
+                <Text>{this.state.dailySteps} / {this.state.stepGoal}</Text>
                 <Progress.Bar progress={this.calculateProgress()} width={200} animated={true}/>
+                <TextInput 
+                    style={styles.text} 
+                    placeholder="Enter Steps" 
+                    placeholderTextColor='ghostwhite'>
+                </TextInput>
+                <TouchableOpacity 
+                    style={styles.button} 
+                    onPress={this.handleStepChange}>
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
             </View>
          );
     }
 }
+
+const styles = StyleSheet.create({
+    text: {
+        textAlign: 'center',
+        fontSize: 30,
+        height: 30,
+        color: 'ghostwhite',
+        fontFamily: 'PressStart2P-Regular'
+    }
+})
  
 export default StepsComponent;
--- containers/PetContainer.js
+++ containers/PetContainer.js
@@ -1,19 +1,34 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import StepsComponent from '../components/StepsComponent.js';
+import EggComponent from '../components/EggComponent';
+import EggWithEarsComponent from '../components/EggWithEarsComponent';
+import SmallPetComponent from '../components/SmallPetComponent';
+import MiddlePetComponent from '../components/MiddlePetComponent';
+import BigPetComponent from '../components/BigPetComponent';
 
 
 class PetContainer extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-
-         }
+            totalSteps: 4000
+        }
+        this.currentPet = this.currentPet.bind(this)
     }
+
+    currentPet(props){
+        console.log('props', this.props.totalSteps);
+
+        if(this.state.totalSteps < 6000) {
+            return <EggWithEarsComponent />
+        }
+    }
+
     render() { 
         return ( 
             <View>
-                <Text>Hello</Text>
+                {this.currentPet()}
                 <StepsComponent />
             </View>
          );
