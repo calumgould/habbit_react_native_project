c2f4c3432f20b7ab97f53fd737261ea8b3844349
mholwill <m_holwill@hotmail.co.uk>
2020-04-28 17:21:50 +0100

    Middle Pet animation wiggle done

--- components/MiddlePetComponent.js
+++ components/MiddlePetComponent.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import {Image, StyleSheet, View, Text, Animated, Easing, TouchableWithoutFeedback, useRef} from 'react-native';
 
 const MiddlePetComponent = () => {
-    return ( 
-        <Image style={styles.image} source={require('../assets/images/boi1_medium.png')} />
-     );
+
+    const animatedValue = new Animated.Value(0)
+
+    const handleAnimation = () => {
+       
+        Animated.loop(
+          Animated.sequence([
+            Animated.timing(animatedValue, {toValue: 1.0, duration: 150, easing: Easing.linear, useNativeDriver: true}),
+            Animated.timing(animatedValue, {toValue: -1.0, duration: 300, easing: Easing.linear, useNativeDriver: true}),
+            Animated.timing(animatedValue, {toValue: 0.0, duration: 150, easing: Easing.linear, useNativeDriver: true})
+          ])
+        , {iterations: 3}).start(); 
+      }
+ 
+        return ( 
+            <View>
+                <TouchableWithoutFeedback onPress={() => handleAnimation()}>
+                    <Animated.Image  style={[{
+                        transform: [{
+                            rotate: animatedValue.interpolate({
+                            inputRange: [-1, 1],
+                            outputRange: ['-0.1rad', '0.1rad'],
+                            })
+                        }]    
+                    }, styles.image]} 
+                    source={require('../assets/images/boi1_medium.png')}  /> 
+                </TouchableWithoutFeedback>
+            </View>
+         );
 }
 
 const styles = StyleSheet.create({
