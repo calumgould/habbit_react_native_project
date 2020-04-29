8ad697e6936487d193fa38102bef4d873705ffae
mholwill <m_holwill@hotmail.co.uk>
2020-04-29 15:59:35 +0100

    Pet Progress bar fixed by tape

--- components/PetComponent.js
+++ components/PetComponent.js
@@ -29,9 +29,27 @@ const PetComponent = (props) => {
             return newTotal = 0
     }
 
+    const resetSteps1 = (growthStageA, growthStageB) => {
+        newTotal = (props.totalSteps - (growthStageA + growthStageB) - 10000)
+        if(newTotal >= 0) {
+            return newTotal
+        } else (newTotal <= 0) 
+            return newTotal = 0
+    }
 
-    const calculateProgress2 = (lastGrowthStage, growthStage) => {
-        return ((props.totalSteps - lastGrowthStage) / growthStage);
+    const calculateProgress2 = (lastGrowthStageA, lastGrowthStageB, growthStage) => {
+        newTotal = lastGrowthStageA + lastGrowthStageB
+        return ((props.totalSteps - lastGrowthStageA) / growthStage);
+    }
+
+    const calculateProgress3 = (lastGrowthStageA, lastGrowthStageB, growthStage) => {
+        newTotal = lastGrowthStageA + lastGrowthStageB
+        return (((props.totalSteps - lastGrowthStageA) - 10000) / growthStage);
+    }
+
+    const calculateProgress4 = (lastGrowthStageA, lastGrowthStageB, growthStage) => {
+        newTotal = lastGrowthStageA + lastGrowthStageB
+        return (((props.totalSteps - lastGrowthStageA) - 40000) / growthStage);
     }
 
     
@@ -43,9 +61,9 @@ const PetComponent = (props) => {
         else if((props.totalSteps - growthStage3) >= growthStage4) {
             return (
                 <View style={styles.view}>
-                <Text style={styles.text}>{resetSteps(growthStage3, growthStage4)} / {growthStage5} </Text>
+                <Text style={styles.text}>{resetSteps1(growthStage3, growthStage4)} / {growthStage5} </Text>
                     <Progress.Bar 
-                    progress={calculateProgress2(growthStage4, growthStage5)}
+                    progress={calculateProgress4(growthStage4, growthStage3, growthStage5)}
                     animated={true}
                     width={150}
                     height={10}
@@ -62,7 +80,7 @@ const PetComponent = (props) => {
                 <View style={styles.view}>
                 <Text style={styles.text}>{resetSteps(growthStage2, growthStage3)} / {growthStage4} </Text>
                     <Progress.Bar 
-                    progress={calculateProgress2(growthStage3, growthStage4)}
+                    progress={calculateProgress3(growthStage3, growthStage2, growthStage4)}
                     animated={true}
                     width={150}
                     height={10}
@@ -79,7 +97,7 @@ const PetComponent = (props) => {
                 <View style={styles.view}>
                     <Text style={styles.text}>{resetSteps(growthStage1, growthStage2)} / {growthStage3} </Text>
                     <Progress.Bar 
-                    progress={calculateProgress2(growthStage2, growthStage3)}
+                    progress={calculateProgress2(growthStage2, growthStage1, growthStage3)}
                     animated={true}
                     width={150}
                     height={10}
